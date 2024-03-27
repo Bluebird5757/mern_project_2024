33681804634afab3a7040aed5f9238b3b3e1ea10
Bluebird5757 <136193863+Bluebird5757@users.noreply.github.com>
2024-03-27 15:26:40 +0530

    deploying

--- config/nikhilconfig.js
+++ config/nikhilconfig.js
@@ -1,2 +1,3 @@
-const server="mongodb://127.0.0.1:27017/NIKHIL2024";
+// const server="mongodb://127.0.0.1:27017/NIKHIL2024";
+const server="mongodb+srv://nikhilgarg288:nikhilmern288@g2c.3misbvo.mongodb.net/NIKHIL2024?retryWrites=true&w=majority&appName=g2c"
 module.exports={server};
